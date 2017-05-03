07a018f209aa45c1ac1a8f17604dab2460e8a61e
emmpak <eiliev05@yahoo.com>
2017-05-03 10:03:22 +0100

    Add restaurant name validation

--- spec/models/restaurants_spec.rb
+++ spec/models/restaurants_spec.rb
@@ -1,6 +1,9 @@
-describe Restaurant do
+require 'rails_helper'
+
+describe Restaurant, type: :model do
   it 'it not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
 
