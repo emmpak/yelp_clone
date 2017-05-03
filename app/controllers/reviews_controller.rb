b6f6ae5fd5bdc4e9d127467ca4d567d071d5bd66
emmpak <eiliev05@yahoo.com>
2017-05-03 10:15:46 +0100

    Add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,6 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @restaurant.reviews.create(review_params)
-
     redirect_to '/restaurants'
   end
 
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -9,8 +9,9 @@ feature 'reviewing' do
     fill_in "Thoughts", with: "so so"
     select '3', from: 'Rating'
     click_button 'Leave Review'
-
     expect(current_path).to eq '/restaurants'
+    click_link 'Chipotle'
     expect(page).to have_content('so so')
+    expect(page).to have_content('3')
   end
 end
